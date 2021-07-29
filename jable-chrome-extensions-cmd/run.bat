b947a99716449733eeade6a5f090b9e98bbfb4e8
Halo <lindaoling@163.com>
2021-07-30 00:29:01 +0800

    init

---
+++ jable-chrome-extensions-cmd/run.bat
@@ -0,0 +1,26 @@
+chcp 936
+@REM chcp 65001
+@REM 936	GB2312
+@REM 20127	US-ASCII
+@REM 65001	UTF-8
+@echo off
+Setlocal enabledelayedexpansion
+
+@REM 协议会触发这个文件
+
+@REM 视频下载目录
+set savepath=C:\Users\Public\Downloads
+set currPath=%~dp0
+set abc=%1
+set param=%abc:~10,-1%
+for /f "tokens=1-2 delims=;" %%a in ("%param%") do (
+	set url=%%a
+	set filename=%%b
+)
+@REM echo url %url%
+@REM echo name %filename%
+for /f "tokens=* delims=" %%# in ('%currPath%\base64.bat -decode %filename%') do set "filename=%%#"
+@REM echo %filename%
+@REM echo %currPath%
+%currPath%\N_m3u8DL-CLI_v2.9.7.exe %url% --saveName "%filename%" --workDir %savepath% --enableDelAfterDone --disableDateInfo
+@REM pause
---
+++ jable-chrome-extensions/content-script.js
@@ -0,0 +1,31 @@
+﻿// 向页面注入script，为了得到页面中js变量
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('inject.js');
+s.onload = function() {this.remove();};
+(document.head || document.documentElement).appendChild(s);
+
+
+
+function Func() {
+	return new Promise((resolve, reject) => {
+		window.addEventListener("message", function(e){
+			resolve(e)
+		}, false);
+	})
+}
+
+Func().then(res => {
+	// console.log('content then');
+	// console.log(res.data);
+	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+		// console.log('content-script addListener');
+		// console.log(request);
+		// console.log(sender);
+		// console.log(res.data);
+		sendResponse(res.data);
+	});
+})
+
+
+
+
