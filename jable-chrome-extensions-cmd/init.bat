b88d765f808e29502dc7a55784dce509ea652742
Halo <lindaoling@163.com>
2021-07-30 22:59:10 +0800

    update

--- jable-chrome-extensions-cmd/init.bat
+++ jable-chrome-extensions-cmd/init.bat
@@ -56,7 +56,7 @@ REG DELETE "HKCR\M3U8dl" /f
 REG ADD "HKCR\M3U8dl" /ve /t REG_SZ /d "M3U8dlProtocol" /f
 REG ADD "HKCR\M3U8dl" /v "URL Protocol" /t REG_SZ /d "%1" /f
 REG ADD "HKCR\M3U8dl\DefaultIcon" /ve /t REG_SZ /d "%1,1" /f
-REG ADD "HKCR\M3U8dl\shell" 
-REG ADD "HKCR\M3U8dl\shell\open" 
+REG ADD "HKCR\M3U8dl\shell" /f
+REG ADD "HKCR\M3U8dl\shell\open" /f
 REG ADD "HKCR\M3U8dl\shell\open\command" /ve /t REG_SZ /d "\"%1\" \"%%1\"" /f
 goto :eof
