27efb9648a18cfc78c5cded39d39731cba190995
Halo <lindaoling@163.com>
2021-07-31 14:51:10 +0800

    添加扩展配置功能，方便配置下载目录

--- jable-chrome-extensions-cmd/run.bat
+++ jable-chrome-extensions-cmd/run.bat
@@ -1,21 +1,16 @@
 @echo off
 Setlocal enabledelayedexpansion
-
 @REM 协议会触发这个文件
 
-@REM 视频下载目录
-set savepath=C:\Users\Public\Downloads
+@REM 当前脚本目录
 set currPath=%~dp0
-set abc=%1
-set param=%abc:~10,-1%
-for /f "tokens=1-2 delims=;" %%a in ("%param%") do (
-	set url=%%a
-	set filename=%%b
-)
-@REM echo url %url%
-@REM echo name %filename%
-for /f "tokens=* delims=" %%# in ('%currPath%\base64.bat -decode %filename%') do set "filename=%%#"
-@REM echo %filename%
-@REM echo %currPath%
-%currPath%\N_m3u8DL-CLI_v2.9.7.exe %url% --saveName "%filename%" --workDir %savepath% --enableDelAfterDone --disableDateInfo
+@REM 接收参数
+set param=%1
+@REM 参数截取
+set param=%param:~10,-1%
+@REM base64解码
+for /f "tokens=* delims=" %%# in ('%currPath%\base64.bat -decode %param%') do set "param=%%#"
+@REM 调起下载程序
+@REM echo %param%
 @REM pause
+%currPath%\N_m3u8DL-CLI_v2.9.7.exe %param%
--- jable-chrome-extensions/content-script.js
+++ jable-chrome-extensions/content-script.js
@@ -1,31 +1,39 @@
 ﻿// 向页面注入script，为了得到页面中js变量
 var s = document.createElement('script');
 s.src = chrome.runtime.getURL('inject.js');
-s.onload = function() {this.remove();};
+s.onload = function() { this.remove(); };
 (document.head || document.documentElement).appendChild(s);
 
 
 
-function Func() {
-	return new Promise((resolve, reject) => {
-		window.addEventListener("message", function(e){
-			resolve(e)
-		}, false);
-	})
-}
+console.log('contents-script.js');
 
-Func().then(res => {
-	// console.log('content then');
-	// console.log(res.data);
-	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-		// console.log('content-script addListener');
-		// console.log(request);
-		// console.log(sender);
-		// console.log(res.data);
-		sendResponse(res.data);
-	});
-})
+chrome.storage.local.get(function(settingsObj) {
+    inputDom = document.createElement('input');
+    inputDom.id = 'ex-settings';
+    inputDom.type = 'hidden';
+    inputDom.value = JSON.stringify(settingsObj);
+    (document.head || document.documentElement).appendChild(inputDom);
+});
 
 
 
+// function Func() {
+//     return new Promise((resolve, reject) => {
+//         window.addEventListener("message", function(e) {
+//             resolve(e)
+//         }, false);
+//     })
+// }
 
+// Func().then(res => {
+//     // console.log('content then');
+//     // console.log(res.data);
+//     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//         // console.log('content-script addListener');
+//         // console.log(request);
+//         // console.log(sender);
+//         // console.log(res.data);
+//         sendResponse(res.data);
+//     });
+// })
